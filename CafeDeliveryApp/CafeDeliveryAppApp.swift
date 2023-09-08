010ac9442042f610ea52f904d74e54a75a394fce
A4reK0v <mablokochanelgame@gmail.com>
2023-09-08 22:37:41 +0300

    Initial Commit

---
+++ CafeDeliveryApp/CafeDeliveryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CafeDeliveryAppApp.swift
+//  CafeDeliveryApp
+//
+//  Created by Андрей Коваленко on 08.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct CafeDeliveryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
